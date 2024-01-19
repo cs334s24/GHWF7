098eaa77a4b70e1908af9c5f453207b7620c98e7
Shawn Ryer <99298681+ShawnRye@users.noreply.github.com>
2024-01-19 13:44:33 -0500

    Revert "added functionality to grades.py and test_grades.py"

--- grades.py
+++ grades.py
@@ -3,8 +3,3 @@ def compute_hw_average(grades):
     if len(grades) == 0:
         return 0
     return grades[0]
-
-def compute_hw_average(grades):
-  if len(grades) == 0:
-      return 0
-  return sum(grades) / len(grades)
